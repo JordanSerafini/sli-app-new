c74fef78b23c078ee1ceeff9fe61c2d5178e4508
JordanSerafini <jordan@solution-logique.fr>
2024-03-04 16:37:49 +0100

    :construction: working

--- src/component/cards/customerCard.tsx
+++ src/component/cards/customerCard.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Customer } from "../../@types/customer";
 
-
 interface Props {
   customer: Customer;
   css: string;
@@ -17,17 +16,19 @@ const CustomerCard: React.FC<Props> = ({ customer, css }) => {
   );
 
   return (
-    <div className={`${css} p-4 rounded-2xl bg-white flex flex-col justify-evenly shadow-effect`}>
-      <div className="text-base flex flex-row justify-between libre-baskerville-regular-italic">
+    <div
+      className={`${css} p-4 rounded-2xl bg-white flex flex-col justify-evenly shadow-effect`}
+    >
+      <div className="text-base flex flex-row justify-between sm: ">
         <h2>{customer.name}</h2>
         <p>{FullName}</p>
       </div>
-      <div className="text-xs flex flex-row justify-between libre-baskerville-regular">
-        {customer.maindeliverycontact_cellphone && (
-          <div>{customer.maindeliverycontact_cellphone}</div>
-        )}
+      <div className="text-xs flex flex-row justify-between ">
         {customer.maininvoicingcontact_phone && (
           <div>{customer.maininvoicingcontact_phone}</div>
+        )}
+        {customer.maindeliverycontact_cellphone && (
+          <div>{customer.maindeliverycontact_cellphone}</div>
         )}
       </div>
     </div>
