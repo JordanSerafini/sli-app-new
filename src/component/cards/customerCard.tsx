0fb0b3d1fe38b009321b1fba727492d6331f9f89
JordanSerafini <jordan@solution-logique.fr>
2024-03-04 15:15:20 +0100

    :sparkles: setup

--- src/component/cards/customerCard.tsx
+++ src/component/cards/customerCard.tsx
@@ -1,10 +1,18 @@
+import React from 'react';
+import { Customer } from '../../@types/customer';
 
-function customerCard({ customer }) {
-  return (
-    <div>
-
-    </div>
-  )
+interface Props {
+  customer: Customer;
+  css: string;
 }
 
-export default customerCard
+const CustomerCard: React.FC<Props> = ({ customer, css }) => {
+    console.log(customer);
+  return (
+    <div>
+      <h2 className={`${css}`} >{customer.name}</h2>
+    </div>
+  );
+}
+
+export default CustomerCard;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,31 @@
+import  { useState } from "react";
+import axios from "axios";
 import CustomerCard from "../component/cards/customerCard";
+import url from "../utils/axios"; 
 
 function Home() {
+  const [customerList, setCustomerList] = useState([]);
 
+  const getCustomers = async () => {
+    try {
+      const response = await axios.get(`${url.local}/customerNew`);
+
+      if (response.data && Array.isArray(response.data.rows)) {
+        setCustomerList(response.data.rows);
+      } else {
+        console.error("La réponse de l'API n'est pas au format attendu.", response);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
-      <CustomerCard />
+      <button onClick={getCustomers}>Get Customers</button>
+      {customerList.map((customer, index) => (
+        <CustomerCard key={index} customer={customer} css="bg-red-500" />
+      ))}
     </>
   );
 }
