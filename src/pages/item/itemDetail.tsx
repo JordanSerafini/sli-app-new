4c4f8ab362e840a7e029429cbdac91b6fae7f504
JordanSerafini <jordan@solution-logique.fr>
2024-03-05 11:58:39 +0100

    :construction: working

--- src/pages/item/itemDetail.tsx
+++ src/pages/item/itemDetail.tsx
@@ -5,6 +5,7 @@ import Badge from "../../component/badge/badge";
 import descriptonLogo from "../../assets/descriptionLogo.png";
 import noteLogo from "../../assets/noteLogo.png";
 import euroLogo from "../../assets/euroLogo.png";
+import unitecentraleIMG from "../../assets/unitecentrale.jpg";
 
 interface ItemDetailProps {
   item: Item;
@@ -12,31 +13,42 @@ interface ItemDetailProps {
 
 function ItemDetail({ item }: ItemDetailProps) {
 
-    let stockBadgeCSS = "";
-    let name = "";
-
-if (item.realstock == 0) {
-    stockBadgeCSS = "bg-red-500 text-white";
-    name="Rupture de stock";
-} else if (item.realstock < 5) {
-    stockBadgeCSS = "bg-orange-500 text-white";
-    name="Stock faible";
-} else {
-    stockBadgeCSS = "bg-green-500 text-white";
-    name="En stock";
+let itemImage = false;
+if (item.caption === "- Unité centrale FUJITSU P420 core i 420") {
+  itemImage = true;
 }
+  
 
+
+    //-----------------   ----------------------
+
+    
+    
+    //-----------------  Gestion Badge ----------------------
+  let stockBadgeCSS = "";
+  let name = "";
+
+  if (item.realstock == 0) {
+    stockBadgeCSS = "bg-red-500 text-white";
+    name = "Rupture de stock";
+  } else if (item.realstock < 5) {
+    stockBadgeCSS = "bg-orange-500 text-white";
+    name = "Stock faible";
+  } else {
+    stockBadgeCSS = "bg-green-500 text-white";
+    name = "En stock";
+  }
 
   return (
     <div className="bg-white h-10/10 p-2 rounded-2xl flex flex-col gap-4">
-      {/*-----------------  1er container: Nom ----------------------
-      <div className="overflow-auto border-b-1 border-black pb-2 text-center bold h-1/10 items-center justify-s flex">
-        <h1>{item.caption}</h1>
+      {/*-----------------  1er container: Nom ----------------------*/}
+      <div className=" h-2/10 overflow-auto border-b-1 border-black pb-2 text-center bold items-center justify-evenly flex flex-row">
+        <h1 className="bg-red-200 overflow-auto max-h-9/10">{item.caption}</h1>
+        {itemImage && <img src={unitecentraleIMG} alt="Image" className="h-9.5/10 w-auto" />}
       </div>
-      */}
 
       {/*-----------------  2eme container: Description/Note  ----------------------*/}
-      <div className="h-8/10 flex flex-col gap-4">
+      <div className="h-6/10 flex flex-col gap-4">
         {/*-----------------  Description ----------------------*/}
         {item.descomclear && (
           <div className="flex flex-row gap-2 h-4.5/10 border-b-1 border-black pb-2">
@@ -47,7 +59,7 @@ if (item.realstock == 0) {
 
         {/*----------------- Note  ----------------------*/}
         {item.notesclear && (
-          <div className="flex flex-row gap-2 h-4.5/10 border-b-1 border-black pb-2">
+          <div className="h-4.5/10 flex flex-row gap-2border-b-1 border-black pb-2">
             <img src={noteLogo} alt="" className="h-6" />
             <p className="max-h-5/10 overflow-auto ">{item.notesclear}</p>
           </div>
@@ -56,18 +68,19 @@ if (item.realstock == 0) {
 
       {/*----------------- 3eme container  ----------------------*/}
       {item.salepricevatincluded && (
-        <div className="h-3/10 flex flex-row gap-2 items-center justify-between">
+        <div className="h-2/10 flex flex-row gap-2 items-center justify-between">
           <div className="flex flex-row gap-2">
             <img src={euroLogo} alt="" className="h-6" />
-            <p className="bold">{item.salepricevatincluded}</p>
+            <p>Prix: {item.salepricevatincluded} €</p>
           </div>
           <div>
             <Badge name={name} css={stockBadgeCSS} />
-            { stockBadgeCSS==="Stock faible" && <p>Attention stock: {item.realstock}</p>}
+            {stockBadgeCSS === "Stock faible" && (
+              <p>Attention stock: {item.realstock}</p>
+            )}
           </div>
         </div>
       )}
-      
     </div>
   );
 }
