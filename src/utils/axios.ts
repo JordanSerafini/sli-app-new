1144c87b78cd3464258f292c5bbdc3c5a615334b
JordanSerafini <jordanserafini.74@gmail.com>
2024-03-09 16:25:16 +0100

    :construction: working

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 const url = {
     local:"http://localhost:5000",
-    heroku:"https://serene-tundra-37919-d1478ece3cff.herokuapp.com", // url api ancienne version
+    heroku:"https://serene-tundra-37919-d1478ece3cff.herokuapp.com", // url api ancienne 
     heroku2:"https://sli-back-964256b21f2d.herokuapp.com/", // bdd heroku pgsql
-    main:"http://localhost:5000",
+    main:"https://sli-back-964256b21f2d.herokuapp.com/",
 }
 export default url;
