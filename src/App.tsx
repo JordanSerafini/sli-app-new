226d75675872215624c1acb1b054647a8989d45e
JordanSerafini <jordan@solution-logique.fr>
2024-03-18 12:00:24 +0100

    workinf

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
 import { DataProvider } from "./context/provider/dataProvider";
 
 import Home from "./pages/home";
@@ -8,22 +8,28 @@ import Login from "./pages/login";
 
 import BottomNavbar from "./component/nav/bottomNavBar";
 import dataContext from "./context/context/dataContext";
+import { useContext } from "react";
 
 function Layout() {
-  const location = useLocation(); 
+  const navigate = useNavigate();
+  const { isLoggedIn } = useContext(dataContext);
+
+  if (!isLoggedIn) {
+    navigate('/login', { replace: true });
+  
+    return null; 
+  }
 
   return (
-    <>
-      <div className="bg-gray-200 h-screen w-screen flex flex-col justify-center items-center">
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/customer" element={<CustomerPage />} />
-          <Route path="/item" element={<ItemPage />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-      {location.pathname !== "/login" && <BottomNavbar />}
-    </>
+    <div className="bg-gray-200 h-screen w-screen flex flex-col justify-center items-center">
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/customer" element={<CustomerPage />} />
+        <Route path="/item" element={<ItemPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <BottomNavbar />
+    </div>
   );
 }
 
