40f8e1ff7bc3568f10522a080928c7b985a145ab
JordanSerafini <jordan@solution-logique.fr>
2024-03-19 09:26:44 +0100

    workinf

--- src/pages/item/itemDetail.tsx
+++ src/pages/item/itemDetail.tsx
@@ -17,6 +17,7 @@ interface ItemDetailProps {
 function ItemDetail({ item }: ItemDetailProps) {
   const [newStockValue, setNewStockValue] = useState(item.realstock);
   const [showModal, setShowModal] = useState(false);
+  const [isSwap, setIsSwap] = useState(false);
   const modalRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -54,8 +55,7 @@ function ItemDetail({ item }: ItemDetailProps) {
     try {
       const response = await axios.post(apiUrl, itemData);
       //console.log("Stock modifié avec succès:", response.data);
-      if (response)
-        setShowModal(false); // Fermer la modal après succès
+      if (response) setShowModal(false); // Fermer la modal après succès
     } catch (error) {
       if (axios.isAxiosError(error)) {
         console.error(
@@ -93,43 +93,77 @@ function ItemDetail({ item }: ItemDetailProps) {
     name = "En stock";
   }
 
+  const handleSwap = () => {
+    setIsSwap(!isSwap);
+  };
+
+  const handleReservationColor = () => {  
+    if (item.stockbookingallowed == 0) {
+      return "bg-red-500";
+      
+    } else {
+      return "bg-green-500";
+    }
+  }
+  console.log(item.stockbookingallowed);
+
+
   return (
     <div className="bg-white h-10/10 p-2 rounded-2xl flex flex-col gap-4">
-      <div className="h-2/10 overflow-auto border-b-1 border-grayblue pb-2 text-center bold items-center justify-evenly flex flex-row">
-        <h1 className="text-grayblue overflow-auto max-h-9/10">
-          {item.caption}
-        </h1>
-        {itemImage && (
-          <img src={unitecentraleIMG} alt="Image" className="h-9.5/10 w-auto" />
-        )}
-      </div>
-
-      <div className="h-6/10 flex flex-col gap-4 text-xs">
-        {item.descomclear && (
-          <div className="flex flex-row gap-2 h-4.5/10 border-b-1 border-grayblue pb-2">
-            <img src={descriptonLogo} alt="" className="h-6" />:
-            <p className="max-h-10/10 overflow-auto ">{item.descomclear}</p>
+      {isSwap ? (
+        <div>
+          <div>
+            <p>réservation:</p>
+            <div className={`h-4 w-4 rounded-full ${handleReservationColor()}`}>
+              
+            </div>
           </div>
-        )}
-
-        {item.notesclear && (
-          <div className="flex flex-row gap-2 h-4.5/10 border-b-1 border-grayblue pb-2">
-            <img src={noteLogo} alt="" className="h-6" />:
-            <p className="max-h-10/10 overflow-auto ">{item.notesclear}</p>
-          </div>
-        )}
-      </div>
-
-      {item.salepricevatincluded && (
-        <div className="h-2/10 flex flex-row gap-2 items-center justify-between">
-          <div className="flex flex-row gap-2">
-            <img src={euroLogo} alt="" className="h-6" />
-            <p>{item.salepricevatincluded}</p>
-          </div>
-          <div className="" onClick={handleStock}>
-            <Badge name={name} css={stockBadgeCSS} />
-          </div>
+          <button onClick={handleSwap}>O</button>
         </div>
+      ) : (
+        <>
+          <div className="h-2/10 overflow-auto border-b-1 border-grayblue pb-2 text-center bold items-center justify-evenly flex flex-row">
+            <button onClick={handleSwap}>O</button>
+            <h1 className="text-grayblue overflow-auto max-h-9/10">
+              {item.caption}
+            </h1>
+            {itemImage && (
+              <img
+                src={unitecentraleIMG}
+                alt="Image"
+                className="h-9.5/10 w-auto"
+              />
+            )}
+          </div>
+
+          <div className="h-6/10 flex flex-col gap-4 text-xs">
+            {item.descomclear && (
+              <div className="flex flex-row gap-2 h-4.5/10 border-b-1 border-grayblue pb-2">
+                <img src={descriptonLogo} alt="" className="h-6" />:
+                <p className="max-h-10/10 overflow-auto ">{item.descomclear}</p>
+              </div>
+            )}
+
+            {item.notesclear && (
+              <div className="flex flex-row gap-2 h-4.5/10 border-b-1 border-grayblue pb-2">
+                <img src={noteLogo} alt="" className="h-6" />:
+                <p className="max-h-10/10 overflow-auto ">{item.notesclear}</p>
+              </div>
+            )}
+          </div>
+
+          {item.salepricevatincluded && (
+            <div className="h-2/10 flex flex-row gap-2 items-center justify-between">
+              <div className="flex flex-row gap-2">
+                <img src={euroLogo} alt="" className="h-6" />
+                <p>{item.salepricevatincluded}</p>
+              </div>
+              <div className="" onClick={handleStock}>
+                <Badge name={name} css={stockBadgeCSS} />
+              </div>
+            </div>
+          )}
+        </>
       )}
 
       {showModal && (
