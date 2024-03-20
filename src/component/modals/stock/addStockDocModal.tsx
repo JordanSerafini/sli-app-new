8109ad87a82f58507118a2b8f19a8ffb5a0049dd
JordanSerafini <jordan@solution-logique.fr>
2024-03-20 09:51:07 +0100

    working on stock

--- src/component/modals/stock/addStockDocModal.tsx
+++ src/component/modals/stock/addStockDocModal.tsx
@@ -72,10 +72,11 @@ function AddStockDocModal({
     console.log("submit");
   };
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-200 bg-opacity-60 w-full">
-      <div className="border-2 border-secondary h-9/10 mb-16 bg-white rounded-2xl z-50 flex flex-col gap- text-gray-600 relative w-11/12 sm:w-4/5">
-      <div className="bg-grayblue rounded-t-xl h-1/10 text-center text-white items-center justify-center flex ">
-        Formulaire ajout document de stock
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-200 bg-opacity-60 w-full h-screen">
+      <div className=" border-2 border-secondary h-9/10 mb-16  rounded-2xl z-50 flex flex-col gap- text-gray-600 relative bg-white w-9.5/10 sm:w-4/5 overflow-hidden ">
+        {/* ------------------------------------ Titre Entete + closeLogo------------------- */}
+        <div className="bg-grayblue rounded-t-xl h-1/10 text-center text-white items-center justify-center flex ">
+          Formulaire ajout document de stock
         </div>
 
         <div className="absolute top-1 right-2">
@@ -83,55 +84,58 @@ function AddStockDocModal({
             <img src={closeLogo} alt="Close" className="h-4 m-" />
           </button>
         </div>
-        <form className="p-2 flex flex-col gap-6 w-10/10" onSubmit={handleSubmit}>
-          <div className="flex flex-col gap-4 w-full items-center">
-          <label className="text-center gap-2 flex flex-row  pt-8 items-center">
+        <form
+          className="p-2 flex flex-col gap-6 w-10/10 h-full overflow-hidden"
+          onSubmit={handleSubmit}
+        >
+          {/* ------------------------------------ Formulaire ------------------- */}
+
+          <div className="flex flex-col gap-4 w-full items-center h-4/10 ">
+            <label className="text-center gap-2 flex flex-row  pt-2 items-center">
               Reference:
               <input
                 type="text"
                 name="reference"
                 value={formData.reference}
                 onChange={handleChange}
-                className="border-1 border-secondary rounded-xl p-2 w-8/10 self-center focus:border-primary focus:outline-none"
+                className="border-1 border-secondary rounded-md p-1 w-8/10 self-center focus:border-primary focus:outline-none"
               />
             </label>
             <div className="w-full flex flex-row justify-between gap-6">
-              
-            <label className="flex gap-2">
-              <select
-                name="documenttype"
-                value={formData.documenttype}
-                onChange={handleChange}
-                className="w-full border-1 border-secondary rounded-md  focus:border-primary focus:outline-none"
-              >
-                <option value="">Selectionner un type</option>
-                <option value="bon_entree">Bon Entrée</option>
-                <option value="bon_livraison">Bon Livraison</option>
-                <option value="inventaire">Inventaire</option>
-                <option value="bon_sortie">Bon Sortie</option>
-                <option value="ordre_transfert">Ordre de Transfert</option>
-                <option value="bon_transfert">Bon Transfert</option>
-              </select>
-            </label>
-            <label className="flex gap-2">
-              <select
-                name="storehouseid"
-                value={formData.storehouseid}
-                onChange={handleChange}
-                className="w-full border-1 border-secondary rounded-md  focus:border-primary focus:outline-none"
-
-              >
-                <option value="">Sélectionner un dépôt</option>
-                {depotAvailable.map((depot) => (
-                  <option key={depot.id} value={depot.id}>
-                    {depot.caption}
-                  </option>
-                ))}
-              </select>
-            </label>
+              <label className="flex gap-2">
+                <select
+                  name="documenttype"
+                  value={formData.documenttype}
+                  onChange={handleChange}
+                  className="w-full border-1 border-secondary rounded-md  focus:border-primary focus:outline-none"
+                >
+                  <option value="">Catégorie :</option>
+                  <option value="bon_entree">Bon Entrée</option>
+                  <option value="bon_livraison">Bon Livraison</option>
+                  <option value="inventaire">Inventaire</option>
+                  <option value="bon_sortie">Bon Sortie</option>
+                  <option value="ordre_transfert">Ordre de Transfert</option>
+                  <option value="bon_transfert">Bon Transfert</option>
+                </select>
+              </label>
+              <label className="flex gap-2">
+                <select
+                  name="storehouseid"
+                  value={formData.storehouseid}
+                  onChange={handleChange}
+                  className="w-full border-1 border-secondary rounded-md  focus:border-primary focus:outline-none"
+                >
+                  <option value="">Dépôt :</option>
+                  {depotAvailable.map((depot) => (
+                    <option key={depot.id} value={depot.id}>
+                      {depot.caption}
+                    </option>
+                  ))}
+                </select>
+              </label>
             </div>
-            
-            <label className="flex gap-2">
+
+            <label className="flex flex-col justify-center items-center w-full">
               Notes:
               <textarea
                 name="notesclear"
@@ -141,35 +145,40 @@ function AddStockDocModal({
               ></textarea>
             </label>
           </div>
-          {/* ------------ sélectionner item à ajouter ------- */}
-          <div className="border-1 border-secondary">
-            <ul className=" min-h-5/10 max-h-5/10 overflow-auto">
-              {itemToAdd.map((item, index) => (
-                <li key={index} className="flex flex-row justify-between p-2 border-b-1 border-secondary ">
+          {/* ------------------------------------ sélectionner item à ajouter ------------------- */}
+          <div className=" max-h-7/10">
+            {/* ------------------------------------ Tableau item a ajouter ------------------- */}
+
+            <div className="border-1 border-secondary p-2 overflow-auto  max-h-6/10  min-h-6/10  ">
+              <div className="">
+                {itemToAdd.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-row overflow-auto justify-between p-2 border-b-1 border-secondary "
+                  >
                     <p>{item.caption}</p>
                     <p>quantité</p>
                     <p>{item.salepricevatincluded}</p>
-                
-                </li>
-                
+                  </div>
+                ))}
+              </div>
+            </div>
+            {/* ------------------------------------ Select et addbtn ------------------- */}
+
+            <select
+              name="selectedItem"
+              value={formData.selectedItem}
+              onChange={handleChange}
+            >
+              {itemList.map((item, index) => (
+                <option key={index} value={item.id}>
+                  {item.caption}
+                </option>
               ))}
-            </ul>
+            </select>
+            <button onClick={handleItemList}>ADD</button>
+            <Button title="Soumettre" onClick={submitClick} css="w-full" />
           </div>
-          <h2>Ajouter un article</h2>
-          {/* Affichage du select avec les éléments de itemList */}
-          <select
-            name="selectedItem"
-            value={formData.selectedItem}
-            onChange={handleChange}
-          >
-            {itemList.map((item, index) => (
-              <option key={index} value={item.id}>
-                {item.caption}
-              </option>
-            ))}
-          </select>
-          <button onClick={handleItemList}>ADD</button>
-          <Button title="Soumettre" onClick={submitClick} css="w-full" />
         </form>
       </div>
     </div>
