f6d35cc9d7b2eaa63871a038a74d39c03e518876
JordanSerafini <jordan@solution-logique.fr>
2024-03-27 16:58:14 +0100

    working on stat page/ sort BE-BS :construction:

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
 import DecoBtn from "../component/button/decoBtn";
-import BottomNavbar from "../component/nav/bottomNavBar";
 
 function Home() {
   const navigate = useNavigate();
@@ -13,7 +12,7 @@ function Home() {
   };
 
   return (
-    <div className="flex flex-col h-screen w-screen items-center justify-start mb-16 gap-4">
+    <div className="flex flex-col h-screen w-screen items-center justify-start gap-4">
       {/*---------------------------------------------------------------------------------------------------------------------------------------------*/}
       <div className="h-1/10 w-7/10 flex flex-row justify-center items-center bg-white border-2 border-secondary mt-2 rounded-lg">
         <h2>
@@ -22,7 +21,7 @@ function Home() {
       </div>
 
       {/*---------------------------------------------------------- Articles -----------------------------------------------------------------------*/}
-      <div className="flex flex-row flex-wrap h-8/10 w-full justify-evenly py-4 gap-2 overflow-auto bg-white">
+      <div className="flex flex-row flex-wrap h-8/10 w-9.5/10 rounded-2xl justify-evenly py-4 gap-2 overflow-auto bg-white">
         <div className="sm:w-4.5/10 w-9/10 bg-red-500 h-4/10 flex flex-row flex-wrap items-center justify-center libre-baskerville-regular text-white tracking-widest cursor-pointer
 ">
           <div
@@ -43,7 +42,7 @@ function Home() {
         </div>
         {/*---------------------------------------------------------Clients ----------------------------------------------------------------------------*/}
 
-        <div className="sm:w-4.5/10 w-9/10 bg-blue-500 h-4/10 flex flex-row flex-wrap items-center justify-center libre-baskerville-regular text-white tracking-widest cursor-pointer">
+        <div className="sm:w-4.5/10 w-9/10 bg-blue-600 h-4/10 flex flex-row flex-wrap items-center justify-center libre-baskerville-regular text-white tracking-widest cursor-pointer">
           <div
             className="w-5/10 h-5/10 border-1 border-white flex items-center justify-center "
             onClick={() => goToPage("customer")}
@@ -62,7 +61,7 @@ function Home() {
         </div>
         {/*------------------------------------------------------ Stock ---------------------------------------------------------------------------*/}
 
-        <div className="sm:w-4.5/10 w-9/10 bg-green-500 h-4/10 flex flex-row flex-wrap items-center justify-center libre-baskerville-regular text-white tracking-widest cursor-pointer
+        <div className="sm:w-4.5/10 w-9/10 bg-green-600 h-4/10 flex flex-row flex-wrap items-center justify-center libre-baskerville-regular text-white tracking-widest cursor-pointer
 ">
           <div
             className="w-5/10 h-5/10 border-1 border-white flex items-center justify-center "
@@ -82,7 +81,7 @@ function Home() {
         </div>
         {/*---------------------------------------------------------------------------------------------------------------------------------------------*/}
 
-        <div className="sm:w-4.5/10 w-9/10 bg-yellow-500 h-4/10 flex flex-row flex-wrap items-center justify-center libre-baskerville-regular text-white tracking-widest cursor-pointer
+        <div className="sm:w-4.5/10 w-9/10 bg-yellow-600 h-4/10 flex flex-row flex-wrap items-center justify-center libre-baskerville-regular text-white tracking-widest cursor-pointer
 ">
           <div
             className="w-5/10 h-5/10 border-1 border-white flex items-center justify-center "
@@ -109,7 +108,6 @@ function Home() {
         <DecoBtn />
       </div>
 
-      <BottomNavbar />
     </div>
   );
 }
