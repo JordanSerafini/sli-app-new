f33b8bea888cb4f7ac1df37af3274b6b7cdcb20a
JordanSerafini <jordan@solution-logique.fr>
2024-03-28 14:34:59 +0100

    working on stat page/ sort BE-BS :construction:

--- src/component/cards/leaflet/leafletAllMap.tsx
+++ src/component/cards/leaflet/leafletAllMap.tsx
@@ -1,26 +1,29 @@
-import React, {  useEffect } from 'react';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
-import L, { divIcon } from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import React, { useEffect } from "react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 interface Props {
   center: { lat: number; lng: number };
   markers: { lat: number; lng: number }[];
-  radius: number; 
+  radius: number;
   zoom?: number;
 }
 
-const leafletAllMap: React.FC<Props> = ({ center, markers, radius, zoom }) => {
-  
+const LeafletAllMap: React.FC<Props> = ({ center, markers, radius, zoom }) => {
   const FilteredMarkers = () => {
     const map = useMap();
 
     useEffect(() => {
+
+      
       map.eachLayer((layer) => {
         if (layer instanceof L.Marker) {
           map.removeLayer(layer);
         }
       });
+
+      map.setView(center, zoom || 15);
 
       markers.forEach((marker) => {
         const distance = map.distance(center, marker);
@@ -28,22 +31,26 @@ const leafletAllMap: React.FC<Props> = ({ center, markers, radius, zoom }) => {
           L.marker([marker.lat, marker.lng]).addTo(map);
         }
       });
-    }, [map]);
-    
+    }, [map, center, markers, radius]);
+
     return null;
   };
 
   return (
-    <div className='h-full w-full'>
-    <MapContainer center={center} zoom={zoom} style={{ height: '400px', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <FilteredMarkers />
-    </MapContainer>
+    <div className="h-full w-full">
+      <MapContainer
+        center={center}
+        zoom={zoom}
+        style={{ height: "400px", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <FilteredMarkers />
+      </MapContainer>
     </div>
   );
 };
 
-export default leafletAllMap;
+export default LeafletAllMap;
--- src/pages/customer/allMap.tsx
+++ src/pages/customer/allMap.tsx
@@ -1,10 +1,9 @@
 import { useContext, useEffect, useState } from "react";
-
 import dataContext from "../../context/context/dataContext";
 import LeafletAllMap from "../../component/cards/leaflet/leafletAllMap";
 import { fetchCustomer } from "../../function/function";
 
-interface markers {
+interface Marker {
   id: number;
   name: string | null | undefined;
   lat: number;
@@ -13,19 +12,33 @@ interface markers {
 
 function AllMap() {
   const { customerList, setCustomerList } = useContext(dataContext);
-  const [markers, setMarkers] = useState<markers[]>([]);
+  const [markers, setMarkers] = useState<Marker[]>([]);
   const [center, setCenter] = useState({ lat: 45.9000002, lng: 6.11667 });
-  const [radius, setRadius] = useState(0.1);
-  const [zoom, setZoom] = useState(17);
+  const [radius, setRadius] = useState(0.3);
+  const [zoom, setZoom] = useState(15);
+  const [address, setAddress] = useState("Annecy");
 
-
-  console.log(radius);
 
   useEffect(() => {
     if (customerList.length === 0) {
       fetchCustomer(setCustomerList);
     }
   }, [setCustomerList, customerList.length]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCenter({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isNaN(radius)) {
+      setRadius(0.1);
+    }
+  }, [radius]);
 
   useEffect(() => {
     const newMarkers = customerList.map((customer) => ({
@@ -38,30 +51,58 @@ function AllMap() {
     setMarkers(newMarkers);
   }, [customerList]);
 
+  const handleAddress = () => {
+    fetch(
+      `https://nominatim.openstreetmap.org/search?q=${address}&format=json&limit=1`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.length > 0) {
+          const newCenter = {
+            lat: parseFloat(data[0].lat),
+            lng: parseFloat(data[0].lon),
+          };
+          setCenter(newCenter);
+        }
+      });
+  };
+
   return (
     <div className="h-screen w-screen flex flex-col justify-start gap-4">
-      <div>
-        <LeafletAllMap
-          center={center}
-          markers={markers}
-          radius={radius}
-          zoom={zoom}
-        />
-      </div>
+      <LeafletAllMap
+        center={center}
+        markers={markers}
+        radius={radius}
+        zoom={zoom}
+      />
       <div className="flex flex-col items-center">
-        <div className="flex flex-row items-center justify-center w-9.5/10 bg-white">
-          <div className="zoom-control">
-            <label htmlFor="zoom-range">Rayon recherche: {radius} km </label>
+        <div className="flex flex-col gap-2 items-center justify-center w-9.5/10 bg-white">
+          <div>
             <input
-              id="zoom-range"
-              type="range"
-              min="0.05"
-              max="10"
-              step="0.5"
-              value={radius}
-              onChange={(e) => setRadius(parseInt(e.target.value, 10))}
+              id="address-input"
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="border-1 border-secondary rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-primary"
             />
+            <button type="button" onClick={handleAddress}>
+              Valider
+            </button>
           </div>
+          <div className="flex flex-row justify-evenly w-full items-center">
+            <label htmlFor="radius-input">Rayon recherche :</label>
+            <div className="flex gap-1 items-center">
+              <input
+                id="radius-input"
+                type="number"
+                value={radius}
+                onChange={(e) => setRadius(parseFloat(e.target.value))}
+                className="border-1 border-secondary rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-primary focus:"
+              />
+              km
+            </div>
+          </div>
+          <div></div>
         </div>
       </div>
     </div>
