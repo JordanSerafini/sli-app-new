15412b2bdafab9f0be24a6dc86d7b88c3f3d73f9
JordanSerafini <jordan@solution-logique.fr>
2024-04-08 11:51:20 +0200

    working on stat page/ sort BE-BS :construction:

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,7 +7,6 @@ import clientLogo from "../assets/clientLogo.png";
 import favLogo from "../assets/favLogo.png";
 import stockLogo from "../assets/stockLogo.png";
 
-import { useAppDispatch, useAppSelector } from "../hooks/redux";
 
 import Icon from "../component/svg/Icon";
 
---
+++ src/store/selectors/isInStock.ts
@@ -0,0 +1,13 @@
+// isInStock.ts
+import { RootState } from "..";
+
+export const isInStockSelector = (
+  state: RootState,
+  itemId: number
+): boolean => {
+  // Recherche l'article par son ID dans la liste des items.
+  const item = state.item.items.find(item => item.id === itemId.toString());
+
+  // Vérifie si le realStock de l'article est supérieur à 0.
+  return !!item && item.realstock > 0;
+};
