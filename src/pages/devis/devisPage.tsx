9f06a2eede83eaf9edf90fd2d8da5fab04e214a2
JordanSerafini <jordan@solution-logique.fr>
2024-04-10 13:17:43 +0200

    rework style of app

--- src/pages/devis/devisPage.tsx
+++ src/pages/devis/devisPage.tsx
@@ -1,70 +1,56 @@
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import useFetch from "../../hooks/useFetch";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { setCustomers } from "../../store/features/customerSlice";
 import { Customer } from "../../types/customer";
 
 interface FetchCustomersResponse {
-  customers: Customer[];
+  rows: Customer[];
 }
 
 function DevisPage() {
-  const limit = 50; // Supposons que cela peut changer en fonction de l'interaction utilisateur
-  const offset = 0; // Idem pour offset
-
   const dispatch = useAppDispatch();
-  const { customers } = useAppSelector((state) => state.customers);
+  // Utilisation du useSelector pour accéder à l'état des clients.
+  const customers = useAppSelector((state) => state.customers.customers);
 
-  // Inclure limit et offset dans les dépendances pour s'assurer que la requête est mise à jour
-  const options = useMemo(
-    () => ({
-      method: "POST",
-      body: JSON.stringify({ limit, offset }),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token") || ""}`,
-      },
-    }),
-    [limit, offset]
-  );
+  // useState pour gérer le client sélectionné.
+  const [client, setClient] = useState<Customer | null>(null);
 
-  const { data, isLoading } = useFetch<FetchCustomersResponse>(
-    "/clients",
-    options
-  );
+  const options = useMemo(() => ({
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token") || ""}`,
+    },
+  }), []); 
+
+  // Utilisation de useFetch pour récupérer les données des clients.
+  const { data, isLoading } = useFetch<FetchCustomersResponse>("/getAllCustomer", options);
 
   useEffect(() => {
+    // Dispatch des clients récupérés vers le Redux store une fois les données chargées.
     if (data) {
-      dispatch(setCustomers(data.customers));
+      dispatch(setCustomers(data.rows));
     }
   }, [data, dispatch]);
 
-  // Gestion de la sélection du client
-  const handleSelectChange = (event:React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedCustomerId = event.target.value;
-    console.log("Client sélectionné ID:", selectedCustomerId);
+  // Gestionnaire pour la sélection d'un client
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedClient = customers.find(customer => customer.id?.toString() === event.target.value);
+    setClient(selectedClient || null);
   };
 
+  //console.log(customers);
+
+  // Condition de chargement intégrée dans le rendu principal pour simplifier.
   if (isLoading) {
-    return (
-      <div className="w-screen h-screen flex flex-col items-center">
-        Loading...
-      </div>
-    );
+    return <div className="w-screen h-screen flex flex-col items-center justify-center">Loading...</div>;
   }
 
   return (
-    <div className="w-screen h-screen flex flex-col items-center">
-      <select onChange={handleSelectChange} defaultValue="">
-        <option value="" disabled>
-          Choisir un client
-        </option>
-        {customers.map((customer) => (
-          <option key={customer.id} value={customer.id}>
-            {customer.name}
-          </option>
-        ))}
-      </select>
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+     
+      {client && <div>Client sélectionné : {client.name}</div>}
     </div>
   );
 }
